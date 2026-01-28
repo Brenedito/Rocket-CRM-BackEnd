06390178f676d014ec2620c470de4baee6aa8f8f
BrenoBritoDev <breno.brito@outlook.com>
2026-01-28 13:19:02 -0300

    add BaseEntity class as a mapped superclass with tenant filtering support

---
+++ src/main/java/com/rocket/crm/models/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.rocket.crm.models;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter
+@Setter
+@FilterDef(name = "tenantFilter", parameters = @ParamDef(name = "tenantId", type = UUID.class))
+@Filter(name = "tenantFilter", condition = "tenant_id = :tenantId")
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+
+    @Column(name = "tenant_id", nullable = false, updatable = false)
+    private UUID tenant_Id;
+}
