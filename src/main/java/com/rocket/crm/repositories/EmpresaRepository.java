f95e98537654b3c474256626999b520be85e875b
BrenoBritoDev <breno.brito@outlook.com>
2026-01-28 13:18:53 -0300

    add repository interfaces for Empresa, Lead, and User with JPA support

---
+++ src/main/java/com/rocket/crm/repositories/EmpresaRepository.java
@@ -0,0 +1,11 @@
+package com.rocket.crm.repositories;
+
+import com.rocket.crm.models.Empresa;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface EmpresaRepository extends JpaRepository<Empresa, UUID> {
+}
