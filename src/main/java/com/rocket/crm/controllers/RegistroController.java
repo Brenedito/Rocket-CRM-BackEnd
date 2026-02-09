8dd959266d1129220672609bdcadef94b55a2a80
BrenoBritoDev <breno.brito@outlook.com>
2026-02-09 13:49:43 -0300

    Remover métodos e dependências do RegistroController

--- src/main/java/com/rocket/crm/controllers/RegistroController.java
+++ src/main/java/com/rocket/crm/controllers/RegistroController.java
@@ -1,13 +1,6 @@
 package com.rocket.crm.controllers;
 
-import com.rocket.crm.dtos.RegistroDTO;
-import com.rocket.crm.services.RegistroService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RegistroController {
 
-    private final RegistroService registroService;
-
-    @PostMapping
-    public ResponseEntity<String> registrar(@Valid @RequestBody RegistroDTO dados) {
-        registroService.registrarNovaEmpresa(dados);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Empresa e Administrador registrados com sucesso!");
-    }
+//    private final RegistroService registroService;
+//
+//    @PostMapping
+//    public ResponseEntity<String> registrar(@Valid @RequestBody RegistroDTO dados) {
+//        registroService.registrarNovaEmpresa(dados);
+//        return ResponseEntity.status(HttpStatus.CREATED).body("Empresa e Administrador registrados com sucesso!");
+//    }
 }
