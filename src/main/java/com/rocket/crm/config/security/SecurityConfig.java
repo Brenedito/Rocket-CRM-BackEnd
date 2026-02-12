a661529280952e3f130ab6f87f442aa4a7842fa6
BrenoBritoDev <breno.brito@outlook.com>
2026-02-12 17:32:29 -0300

    Adicionar cabeçalhos permitidos para CORS no SecurityConfig

--- src/main/java/com/rocket/crm/config/security/SecurityConfig.java
+++ src/main/java/com/rocket/crm/config/security/SecurityConfig.java
@@ -42,7 +42,13 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://salescrm.rocketcompanny.com.br")); // Origem do seu React
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(List.of(
+                "Authorization",
+                "Content-Type",
+                "Accept",
+                "Origin",
+                "X-Requested-With"
+        ));
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
